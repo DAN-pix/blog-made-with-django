9364e6de8419e0c1e52bda3857320f6f2fcc38ab
Dan- <msiternutel@gmail.com>
2020-02-08 16:02:43 -0500

    Mi primer blog con django

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\elkin\documents\python\web_projects\second_project\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
